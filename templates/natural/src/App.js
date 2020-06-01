c57d94db069bd4dadca7b236b559c7b1cae4d6c0
liu-kai <31062816+liu-kai-github@users.noreply.github.com>
2020-06-01 15:16:04 +0800

    update

--- templates/natural/src/App.js
+++ templates/natural/src/App.js
@@ -1,40 +1,62 @@
+import cssStr from './index.css';
 import logo from './logo.svg';
 
-function App() {
-    const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'wrapper');
-    const icon = document.createElement('span');
-    icon.setAttribute('class', 'icon');
-    icon.setAttribute('tabindex', 0);
-    const info = document.createElement('h3');
-    info.setAttribute('class', 'info');
+export default class App extends HTMLElement {
+    constructor() {
+        super();
 
-    info.textContent = 'Hello Freelog !';
+        {{#if enableShadowDom}}
+        const shadow = this.attachShadow({mode: 'open'});
+        {{else}}
+        const shadow = this.attachShadow({mode: 'closed'});
+        {{/if}}
 
-    let imgUrl;
-    if (this.hasAttribute('img')) {
-        imgUrl = this.getAttribute('img');
-    } else {
-        imgUrl = logo;
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'wrapper');
+        const icon = document.createElement('span');
+        icon.setAttribute('class', 'icon');
+        icon.setAttribute('tabindex', 0);
+        const info = document.createElement('h3');
+        info.setAttribute('class', 'info');
+
+        info.textContent = 'Hello Freelog !';
+
+        let imgUrl;
+        if (this.hasAttribute('img')) {
+            imgUrl = this.getAttribute('img');
+        } else {
+            imgUrl = logo;
+        }
+        const img = document.createElement('img');
+        img.src = imgUrl;
+        icon.appendChild(img);
+
+        var style = document.createElement('style');
+
+        style.textContent = cssStr;
+
+        shadow.appendChild(style);
+        shadow.appendChild(wrapper);
+        wrapper.appendChild(icon);
+        wrapper.appendChild(info);
+
+        const info1 = document.createElement('div');
+        info1.textContent = 'Create with Natural';
+        wrapper.appendChild(info1);
+
+        const info2 = document.createElement('div');
+        info2.textContent = 'Build as Web Component';
+        wrapper.appendChild(info2);
+
+        const info3 = document.createElement('div');
+        info3.textContent = 'Use it anywhere!';
+        wrapper.appendChild(info3);
     }
-    const img = document.createElement('img');
-    img.src = imgUrl;
-    icon.appendChild(img);
 
-    wrapper.appendChild(icon);
-    wrapper.appendChild(info);
+    connectedCallback() {
 
-    const info1 = document.createElement('div');
-    info1.textContent = 'Create with Natural';
-    wrapper.appendChild(info1);
-
-    const info2 = document.createElement('div');
-    info2.textContent = 'Build as Web Component';
-    wrapper.appendChild(info2);
-
-    const info3 = document.createElement('div');
-    info3.textContent = 'Use it anywhere!';
-    wrapper.appendChild(info3);
-
-    return wrapper;
+    }
 }
+
+
+
--- templates/natural/src/index.js
+++ templates/natural/src/index.js
@@ -1,29 +1,4 @@
-import cssStr from './index.css';
 import App from './App';
-
-class App extends HTMLElement {
-    constructor() {
-        super();
-
-        {{#if enableShadowDom}}
-        const shadow = this.attachShadow({mode: 'open'});
-        {{else}}
-        const shadow = this.attachShadow({mode: 'closed'});
-        {{/if}}
-
-        const style = document.createElement('style');
-        style.textContent = cssStr;
-        shadow.appendChild(style);
-
-        const wrapper = App();
-        shadow.appendChild(wrapper);
-
-    }
-
-    connectedCallback() {
-
-    }
-}
 
 
 customElements.define('{{name}}', App);
