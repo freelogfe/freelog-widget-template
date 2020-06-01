e49fa53b8e96f87690d8da856e3cc6583b49f8fe
liu-kai <31062816+liu-kai-github@users.noreply.github.com>
2020-06-01 15:09:15 +0800

    重构

---
+++ templates/natural/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+
+function App() {
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'wrapper');
+    const icon = document.createElement('span');
+    icon.setAttribute('class', 'icon');
+    icon.setAttribute('tabindex', 0);
+    const info = document.createElement('h3');
+    info.setAttribute('class', 'info');
+
+    info.textContent = 'Hello Freelog !';
+
+    let imgUrl;
+    if (this.hasAttribute('img')) {
+        imgUrl = this.getAttribute('img');
+    } else {
+        imgUrl = logo;
+    }
+    const img = document.createElement('img');
+    img.src = imgUrl;
+    icon.appendChild(img);
+
+    wrapper.appendChild(icon);
+    wrapper.appendChild(info);
+
+    const info1 = document.createElement('div');
+    info1.textContent = 'Create with Natural';
+    wrapper.appendChild(info1);
+
+    const info2 = document.createElement('div');
+    info2.textContent = 'Build as Web Component';
+    wrapper.appendChild(info2);
+
+    const info3 = document.createElement('div');
+    info3.textContent = 'Use it anywhere!';
+    wrapper.appendChild(info3);
+
+    return wrapper;
+}
--- templates/natural/src/index.js
+++ templates/natural/src/index.js
@@ -1,5 +1,5 @@
 import cssStr from './index.css';
-import logo from './logo.svg';
+import App from './App';
 
 class App extends HTMLElement {
     constructor() {
@@ -11,46 +11,13 @@ class App extends HTMLElement {
         const shadow = this.attachShadow({mode: 'closed'});
         {{/if}}
 
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('class', 'wrapper');
-        const icon = document.createElement('span');
-        icon.setAttribute('class', 'icon');
-        icon.setAttribute('tabindex', 0);
-        const info = document.createElement('h3');
-        info.setAttribute('class', 'info');
-
-        info.textContent = 'Hello Freelog !';
-
-        let imgUrl;
-        if (this.hasAttribute('img')) {
-            imgUrl = this.getAttribute('img');
-        } else {
-            imgUrl = logo;
-        }
-        const img = document.createElement('img');
-        img.src = imgUrl;
-        icon.appendChild(img);
-
-        var style = document.createElement('style');
-
+        const style = document.createElement('style');
         style.textContent = cssStr;
-
         shadow.appendChild(style);
+
+        const wrapper = App();
         shadow.appendChild(wrapper);
-        wrapper.appendChild(icon);
-        wrapper.appendChild(info);
 
-        const info1 = document.createElement('div');
-        info1.textContent = 'Create with Natural';
-        wrapper.appendChild(info1);
-
-        const info2 = document.createElement('div');
-        info2.textContent = 'Build as Web Component';
-        wrapper.appendChild(info2);
-
-        const info3 = document.createElement('div');
-        info3.textContent = 'Use it anywhere!';
-        wrapper.appendChild(info3);
     }
 
     connectedCallback() {
