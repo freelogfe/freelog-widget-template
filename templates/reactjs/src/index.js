94835c036cbba527e49ebf90308d03ecd23dae35
liu-kai <31062816+liu-kai-github@users.noreply.github.com>
2020-06-01 17:44:21 +0800

    添加 reactjs 模板

---
+++ templates/reactjs/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import cssStr from './index.less';
+
+class CustomElement extends HTMLElement {
+    connectedCallback() {
+        const wrapper = document.createElement('div');
+        const shadow = this.attachShadow({mode: 'open'});
+
+        const style = document.createElement('style');
+        style.textContent = cssStr;
+        shadow.appendChild(style);
+        ReactDOM.render(<App/>, wrapper);
+
+        shadow.appendChild(wrapper);
+    }
+}
+
+customElements.define('freelog-hello-0041', CustomElement);
